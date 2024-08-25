9f5123ad2ccedc201747e4d68b7673b092de154f
Aguinaldo Martins <102426405+manoguinas@users.noreply.github.com>
2024-08-25 13:36:21 -0300

    Update SortByDate.java

--- aula_14/SortByDate.java
+++ aula_14/SortByDate.java
@@ -6,7 +6,6 @@ public class SortByDate implements SortStrategy {
 
     @Override
     public void sort(List<User> users) {
-        // Suponha que a classe User tenha uma data de criação para ordenar.
-        Collections.sort(users, Comparator.comparing(User::getId)); // Usando ID como proxy para a data aqui.
+        Collections.sort(users, Comparator.comparing(User::getId));
     }
 }
